7fbebd2e84e931843e73af022bce0e963b3d761d
Andi757 <andrejeichwald@gmx.de>
2017-08-14 19:54:42 +0200

    Splashscreen -fade in and homepage image

--- app/src/main/java/de/hsos/ersti_app/WelcomeActivity.java
+++ app/src/main/java/de/hsos/ersti_app/WelcomeActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.WelcomeStyle);
         setContentView(R.layout.activity_welcome);
+        //Welcome Image which will fade in at the beginning of the app
+        ImageView welcomeImage = (ImageView)findViewById(R.id.welcomeView);
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        welcomeImage.startAnimation(fadeInAnimation);
+        //Welcome-View will be displayed for 3 seconds -> then main-View
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
