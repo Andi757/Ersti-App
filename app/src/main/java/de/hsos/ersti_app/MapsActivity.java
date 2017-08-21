d1909d3314f343a872fe18b0afad9d6c86e23281
Markus <markus.schwengler@hotmail.de>
2017-08-21 16:28:55 +0200

    added toolbat title on mapview

--- app/src/main/java/de/hsos/ersti_app/MapsActivity.java
+++ app/src/main/java/de/hsos/ersti_app/MapsActivity.java
@@ -2,6 +2,7 @@ package de.hsos.ersti_app;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
@@ -13,7 +14,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static de.hsos.ersti_app.R.id.toolbar;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -21,11 +24,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        String title = getIntent().getStringExtra("gps");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        if (title.equals("mensa")) {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Mensa");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("bib")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Bibliothek");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("sl")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("SL-Gebäude");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("si")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("SI-Gebäude");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("val")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Validierungsautomat");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("fit")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Fitnessstudio");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("bus")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Bushaltestelle");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("aa")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("AA-Gebäude");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("aula")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Aula");
+            setSupportActionBar(toolbar);
+        }else if(title.equals("sek")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Studiensekreteriat");
+            setSupportActionBar(toolbar);
+        }else {
+            Toast.makeText(this, "No Layout loaded.", Toast.LENGTH_SHORT).show();
+        }
+
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
