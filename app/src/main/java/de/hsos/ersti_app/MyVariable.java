d2bddaab68e7ba75808c27fc63145d207805fe56
Andi757 <andrejeichwald@gmx.de>
2017-08-22 13:56:22 +0200

    globale Liste

--- app/src/main/java/de/hsos/ersti_app/MyVariable.java
+++ app/src/main/java/de/hsos/ersti_app/MyVariable.java
@@ -2,19 +2,29 @@ package de.hsos.ersti_app;
 
 import android.app.Application;
 
+import java.util.ArrayList;
+
 /**
  * Created by Andrej on 22.08.2017.
  */
 
 public class MyVariable extends Application {
 
-    private int studentVarible = 0;
+    private int studentVariable = 0;
+    public ArrayList<String> checked = new ArrayList<String>();
+
 
     public int getStudentVarible(){
-        return studentVarible;
+        return studentVariable;
     }
 
-    public void setStudentVarible(int increase){
-        this.studentVarible += increase;
+    public void setStudentVarible(String name){
+        for(String s: checked){
+            if(s.equals(name)){
+                this.studentVariable += 10;
+                checked.add(name);
+                break;
+            }
+        }
     }
 }
