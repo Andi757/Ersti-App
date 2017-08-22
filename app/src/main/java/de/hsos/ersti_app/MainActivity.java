89428449296ca70b5809be59482fa6fe5cd5776d
Markus <markus.schwengler@hotmail.de>
2017-08-22 14:02:13 +0200

    some stuff

--- app/src/main/java/de/hsos/ersti_app/MainActivity.java
+++ app/src/main/java/de/hsos/ersti_app/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
