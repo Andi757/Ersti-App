0efcbe2dc42bb8148681b752a722dda085bee438
Andi757 <andrejeichwald@gmx.de>
2017-08-22 16:26:02 +0200

    Liste wird gespeichert

--- app/src/main/java/de/hsos/ersti_app/MyVariable.java
+++ app/src/main/java/de/hsos/ersti_app/MyVariable.java
@@ -1,10 +1,16 @@
 package de.hsos.ersti_app;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.provider.SyncStateContract;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Andrej on 22.08.2017.
@@ -13,14 +19,15 @@ import java.util.ArrayList;
 public class MyVariable extends Application {
 
     private int studentVariable = 0;
-    public ArrayList<String> checked = new ArrayList<String>();
+    public Set<String> checked = new HashSet<String>();
 
 
     //The Variable will be saved either when you close your app
-    public void SaveInt(String key, int value) {
+    public void SaveInt(String key, int value, String list, Set set) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt(key, value);
+        editor.putStringSet(list, set);
         editor.commit();
     }
 
@@ -28,6 +35,12 @@ public class MyVariable extends Application {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         int savedValue = sharedPreferences.getInt("key", 0);
         return savedValue;
+    }
+
+    public Set LoadSet() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Set<String> fetch = sharedPreferences.getStringSet("list", null);
+        return fetch;
     }
 
     public void DeleteInt(){
@@ -38,6 +51,7 @@ public class MyVariable extends Application {
     }
 
 
+
     public int getStudentVariable(){
         return LoadInt();
     }
@@ -46,7 +60,7 @@ public class MyVariable extends Application {
         if(!checked.contains(name)){
             checked.add(name);
             studentVariable += 10;
-            SaveInt("key", studentVariable);
+            SaveInt("key", studentVariable,"list", checked);
         }
     }
     public int getListItem(String name){
