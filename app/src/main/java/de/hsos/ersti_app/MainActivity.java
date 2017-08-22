7699f5a9c8f6c911f2f0d61b7389e4c196f51dd8
Markus <markus.schwengler@hotmail.de>
2017-08-22 15:33:29 +0200

    changes

--- app/src/main/java/de/hsos/ersti_app/MainActivity.java
+++ app/src/main/java/de/hsos/ersti_app/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity
             testView.setImageResource(R.drawable.student_0);
             TextView testText = (TextView)findViewById(R.id.studentText);
             testText.setText("0% Fortschritt - du hast noch viel vor dir!");
-            Toast.makeText(this, "OnResume-"+fortschritt, Toast.LENGTH_SHORT).show();
         }else if(fortschritt==10) {
             ImageView testView = (ImageView)findViewById(R.id.studentView);
             testView.setImageResource(R.drawable.student_10);
