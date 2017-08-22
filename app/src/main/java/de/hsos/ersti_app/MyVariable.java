7a2e1d733dc3d8e2169b4d80b4bd40e4bcd8f8de
Andi757 <andrejeichwald@gmx.de>
2017-08-22 15:11:07 +0200

    blablabla

--- app/src/main/java/de/hsos/ersti_app/MyVariable.java
+++ app/src/main/java/de/hsos/ersti_app/MyVariable.java
@@ -1,6 +1,9 @@
 package de.hsos.ersti_app;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import java.util.ArrayList;
 
@@ -10,18 +13,36 @@ import java.util.ArrayList;
 
 public class MyVariable extends Application {
 
+
     private int studentVariable = 0;
     public ArrayList<String> checked = new ArrayList<String>();
 
 
+
+
+    public void SaveInt(String key, int value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+
+    public int LoadInt(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        int savedValue = sharedPreferences.getInt("key", 0);
+        return savedValue;
+    }
+
     public int getStudentVariable(){
-        return studentVariable;
+        return LoadInt();
     }
 
     public void setStudentVariable(String name){
         if(!checked.contains(name)){
             checked.add(name);
             studentVariable += 10;
+            SaveInt("key", studentVariable);
         }
     }
 }
